d56a38cd659477605a5487dd1e5e8185758c5538
Sumit Kapoor <sumit.india@gmail.com>
2013-09-14 17:42:55 +0530

    code cleanup

--- graphics.js
+++ graphics.js
@@ -1,10 +1,9 @@
+
 function drawBillet(ctx, billet) {
 	
-	//var ctx = canvas.getContext("2d");
 	ctx.fillStyle = billet.color;
 	ctx.fillRect(-billet.length, -billet.diameter/2, billet.length, billet.diameter);
 }
-
 
 function initSimulation() {
 	var cnc = document.getElementById("cnc");
@@ -12,12 +11,27 @@ function initSimulation() {
 	var billet = {color: "#f00", length: 400, diameter: 200};
 	cncCtx.translate(billet.length, cnc.height/2);
 	drawBillet(cncCtx, billet);
+
 	var tool = initTool('tools/tool.bmp');
+
 	moveTool(cncCtx, tool, 0, billet.diameter/2-5, -billet.length, billet.diameter/2-5);
 	//drawTool(cncCtx, tool, {x: 0, z: billet.diameter/2});
 	//drawGhostTool(cncCtx, tool, {x: -50, z: billet.diameter/2})
 }
 
+
+function moveTool(cncCtx, tool, xi, zi, x, z) {
+
+
+	drawGhostTool(cncCtx, tool, {x: xi+1, z: zi});
+	drawTool(cncCtx, tool, {x: xi, z: zi});
+
+	setTimeout(function() {
+		moveTool(cncCtx, tool, xi-1, zi, x, z)
+	}, 20);
+
+
+}
 
 //tool is a Tool object
 //tool.toolCanvas
@@ -43,3 +57,64 @@ function drawGhostTool(ctx, tool, pos){
 	 ctx.drawImage(tool.invertedGhostCanvas, pos.x, pos.z);
 }
 
+function initTool(imgPath){
+	//Creating img tag
+	var toolImg = document.createElement('img');
+	toolImg.src = imgPath;
+	
+	var canvas = document.createElement("canvas");
+	var ctx = canvas.getContext('2d');
+	
+	// drawing tool image on a temp canvas
+	ctx.drawImage(toolImg, 0, 0);
+	
+	var toolData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	var ghostData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+	// Tool: Removing rgb(255,0,255) color from the BMP image and make it transparent
+	// Ghost: Removing rgb(255,0,255) color from the BMP image and make it transparent, and making non magenta color pixel to white (background color)
+	for (var i = 0; i < toolData.data.length; i += 4) {
+		if (toolData.data[i] === 255 && toolData.data[i + 1] === 0 && toolData.data[i + 2] === 255) {
+			toolData.data[i + 3] = 0;
+			ghostData.data[i + 3] = 0;
+		} else {
+			ghostData.data[i] = 255;
+			ghostData.data[i + 1] = 255;
+			ghostData.data[i + 2] = 255;
+			ghostData.data[i + 3] = 255;
+		}
+	}
+	
+	// Making temp canvas to get rid of transparnt issue
+	var toolCanvas = document.createElement("canvas");
+	var toolCanvasCtx = toolCanvas.getContext('2d');
+	toolCanvasCtx.putImageData(toolData, 0, 0);
+
+	// Making temp canvas to get rid of transparnt issue
+	var ghostCanvas = document.createElement("canvas");
+	ghostCanvas.height = toolImg.height;
+	ghostCanvas.width = toolImg.width;
+	var ghostCanvasCtx = ghostCanvas.getContext('2d');
+	ghostCanvasCtx.putImageData(ghostData, 0, 0);
+
+	// Making temp canvas to get rid of transparnt issue
+	var invertedGhostCanvas = document.createElement("canvas");
+	invertedGhostCanvas.height = toolImg.height;
+	invertedGhostCanvas.width = toolImg.width;
+	var invertedGhostCanvasCtx = invertedGhostCanvas.getContext('2d');
+	invertedGhostCanvasCtx.translate(0, toolImg.height);
+	invertedGhostCanvasCtx.scale(1,-1);
+	invertedGhostCanvasCtx.drawImage(ghostCanvas,0,0);
+	
+	
+	// Making tool object
+	var tool = {
+		width: toolImg.width,
+		height: toolImg.height,
+		toolCanvas: toolCanvas,
+		ghostCanvas: ghostCanvas,
+		invertedGhostCanvas: invertedGhostCanvas
+	}
+	
+	return tool;
+}
--- main.js
+++ main.js
@@ -1,178 +1,42 @@
 
 
-var billet = {
-	length: 400,
-	diameter: 200
-}
-
-var tool = {
-	tipx: 0,
-	tipy: 0
-}
-
-
-function initScreen(){
-
-				document.body.style.height = window.innerHeight +'px';
-
-				var windowHeight = window.innerHeight;
-
-				var settings = {
-					toolbarHeight: 30,
-					footerHeight: 30,
-					editorWidth: 300,
-				}
-				// Setting toolbar
-				$('#toolbar').css('height',  settings.toolbarHeight + 'px');
-				
-				// Setting footer	
-				$('#footer').css('height', settings.footerHeight + 'px');
-
-				// Setting editor
-				$('#editor').css('height', (window.innerHeight - settings.toolbarHeight - settings.footerHeight) + 'px');
-				$('#editor').css('width', settings.editorWidth + 'px');
-
-				// Setting simulator
-				$('#simulator').css('height', (window.innerHeight - settings.toolbarHeight - settings.footerHeight) + 'px');
-				$('#simulator').css('width' , (window.innerWidth - settings.editorWidth) + 'px');
-
-				// Setting CNC
-				var cnc = document.createElement('canvas');
-				cnc.setAttribute('id', 'cnc');
-				cnc.height = (window.innerHeight - settings.toolbarHeight - settings.footerHeight);
-				cnc.width = (window.innerWidth - settings.editorWidth);
-				var simulator = document.getElementById('simulator');
-				simulator.appendChild(cnc);
-
-			}
-
-
-
-function loadTool(image) {
-	var tool = document.createElement('img');
-
-	tool.src = 'tools/tool.bmp';
-
-	var c = document.createElement("canvas");
-	c.width = tool.width;
-	c.height = tool.height;
-	var ctx = c.getContext("2d");
-	ctx.drawImage(tool, 0, 0);
-
-	
-
-	var cnc = document.getElementById("cnc");
-
-
-	var cncCtx = cnc.getContext("2d");
-
-
-	// moveTool(toolData, ghostData, cncCtx, 200, 200, 0, 0);
-
-	//cncCtx.clearRect(200, 200, toolData.width, toolData.height);
-
-
-	var tempCanvas = document.createElement("canvas");
-	var tempCtx = tempCanvas.getContext("2d");
-	tempCtx.putImageData(toolData, 0, 0);
-
-	var tempGostCanvas = document.createElement("canvas");
-	var tempGostCtx = tempGostCanvas.getContext("2d");
-	tempGostCtx.putImageData(ghostData, 0, 0);
-
-
-	moveTool(tempCanvas, tempGostCanvas, cncCtx, 200, 200, 0, 0);
-
-	//cncCtx.drawImage(tempCanvas, 200,200);
-
-
-	//cncCtx.putImageData(ghostData,100,200);		 
-
-	//document.getElementById('temp').appendChild(tool);
-}
-
-function moveTool(cncCtx, tool, xi, zi, x, z) {
-
-
-	drawGhostTool(cncCtx, tool, {x: xi+1, z: zi});
-	drawTool(cncCtx, tool, {x: xi, z: zi});
-
-	
-
-	setTimeout(function() {
-		moveTool(cncCtx, tool, xi-1, zi, x, z)
-	}, 0);
-
-
-}
-
 window.onload = function(){
 	initScreen()
 	initSimulation();
 }
 
 
-function initTool(imgPath){
-	//Creating img tag
-	var toolImg = document.createElement('img');
-	toolImg.src = imgPath;
-	
-	var canvas = document.createElement("canvas");
-	var ctx = canvas.getContext('2d');
-	
-	// drawing tool image on a temp canvas
-	ctx.drawImage(toolImg, 0, 0);
-	
-	var toolData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-	var ghostData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+function initScreen(){
 
-	// Tool: Removing rgb(255,0,255) color from the BMP image and make it transparent
-	// Ghost: Removing rgb(255,0,255) color from the BMP image and make it transparent, and making non magenta color pixel to white (background color)
-	for (var i = 0; i < toolData.data.length; i += 4) {
-		if (toolData.data[i] === 255 && toolData.data[i + 1] === 0 && toolData.data[i + 2] === 255) {
-			toolData.data[i + 3] = 0;
-			ghostData.data[i + 3] = 0;
-		} else {
-			ghostData.data[i] = 255;
-			ghostData.data[i + 1] = 255;
-			ghostData.data[i + 2] = 255;
-			ghostData.data[i + 3] = 255;
-		}
+	document.body.style.height = window.innerHeight +'px';
+
+	var windowHeight = window.innerHeight;
+
+	var settings = {
+		toolbarHeight: 30,
+		footerHeight: 30,
+		editorWidth: 300,
 	}
+	// Setting toolbar
+	$('#toolbar').css('height',  settings.toolbarHeight + 'px');
 	
-	// Making temp canvas to get rid of transparnt issue
-	var toolCanvas = document.createElement("canvas");
-	var toolCanvasCtx = toolCanvas.getContext('2d');
-	toolCanvasCtx.putImageData(toolData, 0, 0);
+	// Setting footer	
+	$('#footer').css('height', settings.footerHeight + 'px');
 
-	// Making temp canvas to get rid of transparnt issue
-	var ghostCanvas = document.createElement("canvas");
-	ghostCanvas.height = toolImg.height;
-	ghostCanvas.width = toolImg.width;
-	var ghostCanvasCtx = ghostCanvas.getContext('2d');
-	ghostCanvasCtx.putImageData(ghostData, 0, 0);
+	// Setting editor
+	$('#editor').css('height', (window.innerHeight - settings.toolbarHeight - settings.footerHeight) + 'px');
+	$('#editor').css('width', settings.editorWidth + 'px');
 
-	// Making temp canvas to get rid of transparnt issue
-	var invertedGhostCanvas = document.createElement("canvas");
-	invertedGhostCanvas.height = toolImg.height;
-	invertedGhostCanvas.width = toolImg.width;
-	var invertedGhostCanvasCtx = invertedGhostCanvas.getContext('2d');
-	invertedGhostCanvasCtx.translate(0, toolImg.height);
-	invertedGhostCanvasCtx.scale(1,-1);
-	invertedGhostCanvasCtx.drawImage(ghostCanvas,0,0);
-	
-	
-	// Making tool object
-	var tool = {
-		width: toolImg.width,
-		height: toolImg.height,
-		toolCanvas: toolCanvas,
-		ghostCanvas: ghostCanvas,
-		invertedGhostCanvas: invertedGhostCanvas
-	}
-	
-	return tool;
+	// Setting simulator
+	$('#simulator').css('height', (window.innerHeight - settings.toolbarHeight - settings.footerHeight) + 'px');
+	$('#simulator').css('width' , (window.innerWidth - settings.editorWidth) + 'px');
+
+	// Setting CNC
+	var cnc = document.createElement('canvas');
+	cnc.setAttribute('id', 'cnc');
+	cnc.height = (window.innerHeight - settings.toolbarHeight - settings.footerHeight);
+	cnc.width = (window.innerWidth - settings.editorWidth);
+	var simulator = document.getElementById('simulator');
+	simulator.appendChild(cnc);
+
 }
-
-
-
